eb182b52f792389665ed39ae05e6c1f9ae25988e
acostapcamila <50851052+acostapcamila@users.noreply.github.com>
2025-07-14 22:53:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,39 @@
-function guardarEstado() {
-  localStorage.setItem('estadoMaterias', JSON.stringify(materias));
-}
-
-function cargarEstado() {
-  const datos = localStorage.getItem('estadoMaterias');
-  if (datos) {
-    const materiasGuardadas = JSON.parse(datos);
-    materiasGuardadas.forEach((mGuardada, i) => {
-      materias[i].aprobado = mGuardada.aprobado;
-      materias[i].nota = mGuardada.nota;
-    });
-  }
-}
 const materias = [
   { codigo: 'Int1', nombre: 'Integración I', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'IngSoc', nombre: 'Ingeniería y Sociedad', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'AlgGeo', nombre: 'Álgebra y Geometría Analítica', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'AM1', nombre: 'Análisis Matemático I', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'AM2', nombre: 'Análisis Matemático II', correlatividad: ['AM1'], aprobado: false, nota: null },
-  { codigo: 'QG', nombre: 'Química General', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'SR', nombre: 'Sistemas de Representación', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'FI', nombre: 'Fundamentos de Informática', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'Ingeniería y Sociedad', nombre: 'Ingeniería y Sociedad', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'AlgGeometría', nombre: 'Álgebra y Geometría Analítica', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'AnalMat1', nombre: 'Análisis Matemático I', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'AnalMat2', nombre: 'Análisis Matemático II', correlatividad: ['AnalMat1'], aprobado: false, nota: null },
+  { codigo: 'QuímicaGen', nombre: 'Química General', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'SistemasRep', nombre: 'Sistemas de Representación', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'FundInformatica', nombre: 'Fundamentos de Informática', correlatividad: [], aprobado: false, nota: null },
   { codigo: 'Int2', nombre: 'Integración II', correlatividad: ['Int1'], aprobado: false, nota: null },
-  { codigo: 'PE', nombre: 'Probabilidad y Estadística', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'QI', nombre: 'Química Inorgánica', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'F1', nombre: 'Física I', correlatividad: ['AM1'], aprobado: false, nota: null },
-  { codigo: 'F2', nombre: 'Física II', correlatividad: ['F1'], aprobado: false, nota: null },
-  { codigo: 'QO', nombre: 'Química Orgánica', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'I1', nombre: 'Inglés I', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'MSA', nombre: 'Matemática Superior Aplicada', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'ProbEst', nombre: 'Probabilidad y Estadística', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'QInorg', nombre: 'Química Inorgánica', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'Fisica1', nombre: 'Física I', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'Fisica2', nombre: 'Física II', correlatividad: ['Fisica1'], aprobado: false, nota: null },
+  { codigo: 'QOrganica', nombre: 'Química Orgánica', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'Ingles1', nombre: 'Inglés I', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'MatSup', nombre: 'Matemática Superior Aplicada', correlatividad: [], aprobado: false, nota: null },
   { codigo: 'Int3', nombre: 'Integración III', correlatividad: ['Int2'], aprobado: false, nota: null },
   { codigo: 'Termo', nombre: 'Termodinámica', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'Eco', nombre: 'Economía', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'Leg', nombre: 'Legislación', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'MEI', nombre: 'Mecánica Eléctrica Industrial', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'FQ', nombre: 'Físico Química', correlatividad: ['F2'], aprobado: false, nota: null },
-  { codigo: 'FT', nombre: 'Fenómenos de Transporte', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'QA', nombre: 'Química Analítica', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'I2', nombre: 'Inglés II', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'Economia', nombre: 'Economía', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'Legislacion', nombre: 'Legislación', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'MecanicaElectrica', nombre: 'Mecánica Eléctrica Industrial', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'FisicoQ', nombre: 'Físico Química', correlatividad: ['Fisica2'], aprobado: false, nota: null },
+  { codigo: 'FenTransporte', nombre: 'Fenómenos de Transporte', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'QAnalitica', nombre: 'Química Analítica', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'Ingles2', nombre: 'Inglés II', correlatividad: [], aprobado: false, nota: null },
   { codigo: 'Int4', nombre: 'Integración IV', correlatividad: ['Int3'], aprobado: false, nota: null },
-  { codigo: 'OU1', nombre: 'Operaciones Unitarias I', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'TET', nombre: 'Tecnología de la Energía Térmica', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'Bio', nombre: 'Biotecnología', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'OU2', nombre: 'Operaciones Unitarias II', correlatividad: ['OU1'], aprobado: false, nota: null },
-  { codigo: 'IRQ', nombre: 'Ing. de las Reacciones Químicas', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'CEP', nombre: 'Control Estadístico de Procesos', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'OI', nombre: 'Organización Industrial', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'CAP', nombre: 'Control Automático de Procesos', correlatividad: [], aprobado: false, nota: null },
-  { codigo: 'PF', nombre: 'Proyecto Final - Integración V', correlatividad: ['Int4'], aprobado: false, nota: null }
+  { codigo: 'OperUnit1', nombre: 'Operaciones Unitarias I', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'EnergiaTermica', nombre: 'Tecnología de la Energía Térmica', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'Biotecnologia', nombre: 'Biotecnología', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'OperUnit2', nombre: 'Operaciones Unitarias II', correlatividad: ['OperUnit1'], aprobado: false, nota: null },
+  { codigo: 'ReaccionesQuimicas', nombre: 'Ingeniería de las Reacciones Químicas', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'ControlProcesos', nombre: 'Control Estadísticos de Procesos', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'OrganizacionIndustrial', nombre: 'Organización Industrial', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'ControlAutomatico', nombre: 'Control Automático de Procesos', correlatividad: [], aprobado: false, nota: null },
+  { codigo: 'ProyectoFinal', nombre: 'Proyecto Final - Integración V', correlatividad: ['Int4'], aprobado: false, nota: null }
 ];
 
 function puedeDesbloquear(materia) {
@@ -112,23 +98,13 @@ function desbloquearRequisitos() {
 }
 
 function init() {
-  cargarEstado();
   materias.forEach(m => {
     const materiaDiv = crearMateria(m);
     container.appendChild(materiaDiv);
-
     if (puedeDesbloquear(m)) {
       materiaDiv.classList.remove('disabled');
     }
-
-    if (m.aprobado) {
-      actualizarMateria(m);
-      const notaInput = materiaDiv.querySelector('.note-box');
-      notaInput.value = m.nota || '';
-      notaInput.style.pointerEvents = 'auto';
-    }
   });
-}
 }
 
 init();
